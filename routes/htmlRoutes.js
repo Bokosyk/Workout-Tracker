046b43fccdd9c242d5e09dfdd03e605a8b284a5f
Leandro Parrado <lparrado321@gmail.com>
2021-02-15 21:10:15 -0500

    Added new html route in htmlroutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -6,9 +6,13 @@ router.get('/exercise', (req, res) => {
     res.sendFile(path.join(_dirname, "../public/exercise.html"))
 });
 
-// Connects router to exercise.html
+// Connects router to stats.html
 router.get('/stats', (req, res) => {
     res.sendFile(path.join(_dirname, "../public/stats.html"))
+});
+// Connects router to index.html
+router.get('/index', (req, res) => {
+    res.sendFile(path.join(_dirname, "../public/index.html"))
 });
 
 // Makes files globally available
